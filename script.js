574fa9a4807a55bf957e3a9fc7354b5b1030d275
Piyush Jain <126599802+piyushjain5350@users.noreply.github.com>
2024-02-12 21:55:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 const isPowerOfTwo = (number) => {
   //your code here
+	if(number<=0){
+		return false;
+	}
+	
+	while(number>1){
+		if(number%2===0){
+			number=number/2;
+		}else{
+			return false;
+		}
+	}
+	return true;
 };
 
 const num = parseInt(prompt("Enter a number"));
-alert(isPowerOfTwo(num));
+alert(isPowerOfTwo(num));   
+ 
